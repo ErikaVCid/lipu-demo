c2348728dd1297144793fd69c7af1b249f23e183
Erika Valenzuela <e.valenzuela@lidcorp.mx>
2026-04-23 12:11:03 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'lipu-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg',
+  'https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:wght@300;400;500&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function(){});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        if(response && response.status === 200 && response.type === 'basic') {
+          var clone = response.clone();
+          caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
+        }
+        return response;
+      }).catch(function(){
+        return cached;
+      });
+    })
+  );
+});
